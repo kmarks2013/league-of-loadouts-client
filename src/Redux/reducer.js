bfcb60b0993f84821ee7ae11f748f6a5e5c57a30
kmarks2013 <kmarks2013@gmail.com>
2019-11-01 18:55:01 -0400

    set up store and reducer for login

---
+++ src/Redux/reducer.js
@@ -0,0 +1,15 @@
+const initialState = {
+
+}
+
+export default (state = initialState, { type, payload }) => {
+    switch (type) {
+        case 'SET_USER':
+          console.log('reducer: ', payload);
+          return payload;
+        case 'CLEAR_USER':
+          return {};
+        default:
+          return state;
+      }
+}
