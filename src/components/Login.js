cf2d3eb2f14f87a519461b95c76824a18c1fd2bb
kmarks2013 <kmarks2013@gmail.com>
2019-11-01 17:09:51 -0400

    login and signup button in nav bar created and consolelog events

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,15 +2,20 @@ import React, { Component } from 'react'
 
 export default class Login extends Component {
 
-    handleLoginClick = () => {
-        console.log(i should ope)
+    loginClick = () => {
+        console.log('i should open up a login form')
     }
 
+    signUpClick = () => {
+        console.log('i should open up a sign up form')
+    }
+    
     render() {
         return (
             <div>
-                <button>Login</button>
-                <button>Sign-up</button>
+                <button onClick={this.loginClick}>Login</button>
+                <button onClick={this.signUpClick}>Sign-up</button>
+                <p>When a user is logged in i should then render a make a new loadout button and a username and a logout button</p>
             </div>
         )
     }
