5afa39fb5c70369b548fd20a701544c733e4f3c3
kmarks2013 <kmarks2013@gmail.com>
2019-11-01 20:29:18 -0400

    set up login fetch and persist fetch

---
+++ src/Redux/actions.js
@@ -0,0 +1,43 @@
+
+const setUserAction = userObj => ({
+    type: 'SET_USER',
+    payload: userObj
+})
+
+const clearUserAction = () => ({
+    type: "CLEAR_USER"
+})
+
+const persistUserFromAPI = () => dispatch => {
+    fetch('http://localhost:3000/persist' , {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: 'bearer' + localStorage.token
+        }
+    })
+    .then(res = res.json())
+    .then(user => {
+        dispatch(setUserAction(user))
+    })
+}
+
+const loginUserToDB = userData => dispatch => {
+    fetch('http://localhost:3000/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(userData)
+    })
+    .then(res => res.json())
+    .then(data =>{
+        dispatch(setUserAction(data.user))
+        localStorage.token = data.token
+    })
+}
+
+export default {
+    persistUserFromAPI,
+    loginUserToDB,
+    clearUserAction
+}
