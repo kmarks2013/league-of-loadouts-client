77e25787b0e280a01fe8dbf9c408efd6a34deef5
kmarks2013 <kmarks2013@gmail.com>
2019-11-20 08:24:47 -0500

    css styling

--- src/components/items/ItemCard.js
+++ src/components/items/ItemCard.js
@@ -8,13 +8,17 @@ class ItemCard extends Component {
            return (
             <div className='content-container'>
                 <div className='item-card'>
-                    <h2>{item.name }</h2>
-                    <img src={`/items_images/${item.api_num}.png`} alt=''/>
-                    <h3>ItemDescription</h3>
-                    <p>{item.description}</p>
-                    <p>{item.plaintext}</p>
-                    <h3>Cost</h3>
-                    <p>{item.cost}</p>
+                    <div className='item-icon'>
+                        <h2>{item.name }</h2>
+                        <img src={`/items_images/${item.api_num}.png`} alt=''/>
+                    </div>
+                    <div className='item-info'>
+                        <h3>ItemDescription</h3>
+                        <p>{item.description}</p>
+                        <p>{item.plaintext}</p>
+                        <h3>Cost</h3>
+                        <p>{item.cost}</p>
+                    </div>
                 </div>
             </div>
            )
