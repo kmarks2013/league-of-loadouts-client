53f8456323b5f442f78482b625b994a4e48345c5
kmarks2013 <kmarks2013@gmail.com>
2019-11-25 16:33:21 -0500

    serach feature works

--- src/components/champions/ChampionList.js
+++ src/components/champions/ChampionList.js
@@ -3,17 +3,39 @@ import { connect } from 'react-redux'
 import ChampionTile from './ChampionTile'
 
 class ChampionList extends Component {
+    state = {
+        searchValue:'',
+    }
+
+    
+    handleSearch = (evt) => {
+        console.log(evt.target.value, this.state.searchValue)
+        this.setState({
+            searchValue: evt.target.value
+        })
+    }
+
+
+
     renderCharacterTiles = () => {
-        return this.props.champions.map(champion => {
+        const serachedChampions = this.props.champions.filter( champion => {
+           return champion.name.toLowerCase().indexOf(this.state.searchValue.toLowerCase()) !== -1
+        })
+
+        return serachedChampions.map(champion => {
           return  <ChampionTile key={champion.id} champion={champion} />
         })
     }
 
     render() {
+        console.log(this.state.filteredChampions)
+
         return (
             <div className='content-container'>
                 <div className='content-tiles'>
                 <h1> All Champions </h1> 
+                    <label>Search</label>
+                    <input type='text' value={this.state.searchValue} onChange={this.handleSearch}></input> <br></br>
                     {this.renderCharacterTiles()}
                 </div>
             </div>
