84b97c65fb63d4d290f4cb2063c80941017a73d6
kmarks2013 <kmarks2013@gmail.com>
2020-03-01 15:50:38 -0500

    adds ChampionInfo component

---
+++ src/components/champions/championCardInfo/ChampionInfo.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class ChampionInfo extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
