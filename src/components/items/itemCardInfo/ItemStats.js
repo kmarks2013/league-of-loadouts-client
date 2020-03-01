74b574b8b1ee55f8076e1c5745d24105fa8049f5
kmarks2013 <kmarks2013@gmail.com>
2020-03-01 16:06:11 -0500

    Copies item stats into Item Stat component

--- src/components/items/itemCardInfo/ItemStats.js
+++ src/components/items/itemCardInfo/ItemStats.js
@@ -2,8 +2,12 @@ import React from 'react'
 
 function ItemStats() {
     return (
-        <div>
-            
+        <div className='item-info'>
+            <h3>ItemDescription</h3>
+            <p>{item.description}</p>
+            <p>{item.plaintext}</p>
+            <h3>Cost</h3>
+            <p>{item.cost}</p>
         </div>
     )
 }
